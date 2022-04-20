9ca00d5f7a45c484c9a93a02ccebe76b6adae727
Paul-VS-prog <74456837+Paul-VS-prog@users.noreply.github.com>
2022-04-20 16:46:16 +0300

    IndexServlet

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -3,16 +3,32 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/home")
 public class MainServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Integer visitCounter = (Integer) session.getAttribute("visitCounter");
+        if (visitCounter == null) {
+            visitCounter = 1;
+        } else {
+            visitCounter++;
+        }
+        session.setAttribute("visitCounter", visitCounter);
+        String username = req.getParameter("username");
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Home!");
+        if (username == null) {
+            printWriter.write("Hello, Anonymous" + "<br>");
+        } else {
+            printWriter.write("Hello, " + username + "<br>");
+        }
+        printWriter.write("Page was visited " + visitCounter + " times.");
         printWriter.close();
     }
 }
